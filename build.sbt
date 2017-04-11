132e5927bffe837037f769c740e734dcdbbab8b9
Jonathan Frawley <jmf@9lines.org>
2017-04-11 22:46:26 +0100

    Add left pad dep and works fine

--- build.sbt
+++ build.sbt
@@ -1,7 +1,10 @@
 enablePlugins(ScalaJSPlugin)
+enablePlugins(ScalaJSBundlerPlugin)
 
 name := "Scala.js Tutorial"
 scalaVersion := "2.12.1" // or any other Scala version >= 2.10.2
 
 // This is an application with a main method
 scalaJSUseMainModuleInitializer := true
+
+npmDependencies in Compile += "left-pad" -> "1.1.3"
--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -1,9 +1,17 @@
 package tutorial.webapp
 
+import scala.scalajs.js
 import scala.scalajs.js.JSApp
+import scala.scalajs.js.annotation.JSImport
+
+@JSImport("left-pad", JSImport.Namespace)
+@js.native
+object LeftPad extends js.Object {
+  def apply(str: String, n: Int): String = js.native
+}
 
 object TutorialApp extends JSApp {
   def main(): Unit = {
-    println("Hello world!")
+    println(LeftPad("123", 5))
   }
 }
